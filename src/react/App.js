df46eb6b2309a456a8eeeebbdda984d04dc98fae
James Spence <Jamesaspence20@gmail.com>
2020-04-18 14:32:24 -0500

    now full height of window

--- src/react/App.js
+++ src/react/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.scss';
+import Header from './common/Header';
+import Footer from './common/Footer';
 import { Switch, Route } from 'react-router-dom';
 import ActiveListContainer from './list/ActiveListContainer';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route path="/" component={ActiveListContainer} />
       </Switch>
+      <Footer/>
     </div>
   );
 }
