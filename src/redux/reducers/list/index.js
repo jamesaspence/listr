fd8a5a6e2603fb129e8268298b8849405a765d0d
James Spence <Jamesaspence20@gmail.com>
2020-04-18 02:02:47 -0500

    cleared test list items

--- src/redux/reducers/list/index.js
+++ src/redux/reducers/list/index.js
@@ -6,16 +6,7 @@ export const DEFAULT_LIST_ID = 12345;
 
 export const DEFAULT_STATE = {
   lists: {
-    [DEFAULT_LIST_ID]: [
-      {
-        text: 'Pizza',
-        checked: false
-      },
-      {
-        text: 'Bacon',
-        checked: true
-      }
-    ]
+    [DEFAULT_LIST_ID]: []
   },
   activeList: DEFAULT_LIST_ID
 };
