8b62e51d5c5d1daa7b7f8ec6dd520f1ecbb9c91e
James Spence <Jamesaspence20@gmail.com>
2020-04-18 16:04:45 -0500

    removed need for toJson in tests, updated App snapshot

--- src/react/App.test.js
+++ src/react/App.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 import App from './App';
 
 it('matches snapshot', () => {
-  expect(toJson(shallow(<App/>))).toMatchSnapshot();
+  expect(shallow(<App/>)).toMatchSnapshot();
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -5,5 +5,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { createSerializer } from 'enzyme-to-json';
 
+expect.addSnapshotSerializer(createSerializer());
 configure({ adapter: new Adapter() });
