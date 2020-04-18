f52d466a30869a83811b776f71f27e4811adb942
James Spence <Jamesaspence20@gmail.com>
2020-04-18 00:21:54 -0500

    cleaned up cra initial html / styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './react/App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
