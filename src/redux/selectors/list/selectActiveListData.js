eb8756e0c3ea5e41efee9311c9896f454bc50d8b
James Spence <jamesaspence20@gmail.com>
2020-05-17 01:02:42 -0500

    unit test for new selector

--- src/redux/selectors/list/selectActiveListData.js
+++ src/redux/selectors/list/selectActiveListData.js
@@ -1,7 +1,6 @@
 import selectActiveList from './selectActiveList';
 import selectActiveListId from './selectActiveListId';
 
-//TODO write unit tests
 const selectActiveListData = state => ({
   items: selectActiveList(state),
   listId: selectActiveListId(state)
