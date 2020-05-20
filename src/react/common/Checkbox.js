222b713c6d56434365de0de8fbfc1744db586dda
James Spence <jamesaspence20@gmail.com>
2020-05-19 19:01:29 -0500

    separated out checkbox component
    ItemCheckbox is now Checkbox-specific, and I can create a checkbox for toggle / untoggle all.

---
+++ src/react/common/Checkbox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Checkbox.scss';
+
+const Checkbox = ({
+  className = null,
+  id,
+  checked = false,
+  onChange
+}) => {
+  return (
+    <div className={`Checkbox ${className}`} onClick={onChange}>
+      <input className="Checkbox__toggle" type="checkbox" id={id} onChange={onChange} checked={checked}/>
+      <label className="Checkbox__toggleLabel" htmlFor={id} />
+    </div>
+  );
+};
+
+export default Checkbox;
