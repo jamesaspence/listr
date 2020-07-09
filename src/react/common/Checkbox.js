a0824aed69fc0fc742aa1da6f407dd2369f15efc
James Spence <jamesaspence20@gmail.com>
2020-07-09 00:39:45 -0500

    added checked prop back to input

--- src/react/common/Checkbox.js
+++ src/react/common/Checkbox.js
@@ -9,7 +9,7 @@ const Checkbox = ({
 }) => {
   return (
     <div className={`Checkbox ${className}`}>
-      <input type="checkbox" className="Checkbox__hidden" id={id} onChange={onChange}/>
+      <input type="checkbox" className="Checkbox__hidden" id={id} checked={checked} onChange={onChange}/>
       <label htmlFor={id} className={`Checkbox__styled ${checked ? 'Checkbox__checked' : ''}`}>
         <svg className="Checkbox__icon" viewBox="0 0 24 24">
           <polyline points="20 6 9 17 4 12" />
