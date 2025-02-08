56798c91f42894eec56200f8ae9a944c5aa8c9c5
James Spence <jamesaspence@users.noreply.github.com>
2025-02-07 19:17:10 -0600

    feat: Add delete all / delete checked buttons (#69)
    * feat: add overlay element with position:absolute
    
    * feat: initial delete confirmation modal
    
    * feat: initial deletion modal appearance from clicks
    
    * feat: deleteItems reducer / action
    
    * feat: working dispatch of delete items action
    
    * feat: only show checked deletion if items checked

--- src/react/views/ListView.js
+++ src/react/views/ListView.js
@@ -5,12 +5,17 @@ import GridLayout from '../common/layout/GridLayout';
 import NewListItemPromptContainer from '../list/create/NewListItemPromptContainer';
 import ActiveListContainer from '../list/ActiveListContainer';
 import CheckAllToggle from '../list/CheckAllToggle';
+import DeleteItemsContainer from '../list/DeleteItemsContainer';
 
 const ListView = () => {
   const itemCount = useSelector(selectActiveListCount);
 
   return (
     <GridLayout className="ListView">
+      {
+        itemCount > 0 &&
+        <DeleteItemsContainer />
+      }
       <CheckAllToggle id={'CheckAllToggle--top'} />
       <ActiveListContainer />
       {
