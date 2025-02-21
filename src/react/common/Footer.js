f470e9fe764dd8b683e920d79943b80144547baa
James Spence <jamesaspence20@gmail.com>
2025-02-21 00:10:53 -0600

    feat: add author attribution to footer

--- src/react/common/Footer.js
+++ src/react/common/Footer.js
@@ -6,6 +6,9 @@ const Footer = () => (
     <small className="Attribution">
       Logo made by <a href="https://www.flaticon.com/authors/vectors-market" className="Attribution__link" title="Vectors Market">Vectors Market</a> from <a href="https://www.flaticon.com/" className="Attribution__link" title="Flaticon"> www.flaticon.com</a>
     </small>
+    <small className="Attribution">
+      Made with love by <a href="https://www.jamesspence.dev/" target="_blank" className="Attribution__link">James Spence</a>
+    </small>
   </footer>
 );
 
